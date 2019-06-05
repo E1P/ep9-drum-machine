dbd64e4bc1ceb0a8978beebfbb551ed9a44174a2
E1P <ewan.pearce@gmail.com>
2019-06-05 17:40:18 +0100

    Add channel div and set meter level to zero once sample drops below -50

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Tone from "tone";
 import LevelMeter from "./Components/LevelMeter";
 
 function App() {
-  console.log("Mounting...");
   const [amplitude, setAmplitude] = useState(-50);
   const [playing, setPlaying] = useState(false);
   const [Sampler, setSampler] = useState(null);
@@ -12,7 +11,7 @@ function App() {
 
   useEffect(() => {
     console.log("Sampler useEffect triggered...");
-    !Sampler && setSampler(new Tone.Sampler({ C3: "./beat-radar-sounds/stab.wav" }).toMaster());
+    !Sampler && setSampler(new Tone.Sampler({ C3: "./beat-radar-sounds/kick.wav" }).toMaster());
     !Meter && setMeter(new Tone.Meter());
     Sampler && Meter && Sampler.connect(Meter);
   }, [Sampler, Meter]);
@@ -36,20 +35,22 @@ function App() {
   };
 
   useEffect(() => {
-    console.log("render!!!");
     const getLevel = () => {
       const level = Math.trunc(Meter.getLevel());
       if (level > -50) setAmplitude(level);
+      else if (amplitude !== -50 && level < -50) setAmplitude(-50);
       requestAnimationFrame(getLevel);
     };
     Meter && requestAnimationFrame(getLevel);
     // return;
-  }, [Meter]);
+  }, [Meter, amplitude]);
 
   return (
     <div className="App" onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex={0}>
-      <button onClick={playSound}>BEATS!</button>
-      <LevelMeter level={amplitude} />
+      <div className="channel">
+        <button onClick={playSound}>BEATS!</button>
+        <LevelMeter level={amplitude} />
+      </div>
     </div>
   );
 }
