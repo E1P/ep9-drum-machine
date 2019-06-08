c850c085143b43919f7db7bab9ce5d94c2324cc8
E1P <ewan.pearce@gmail.com>
2019-06-08 20:41:09 +0100

    Completely refactor component with hooks to effect async reverb loading

--- src/Components/MasterOut.jsx
+++ src/Components/MasterOut.jsx
@@ -1,17 +1,42 @@
+import { /* React, */ useState, useEffect } from "react";
 import Tone from "tone";
+Tone.context.lookAhead = 0;
 
-const MasterOut = () => {
-  const masterCompressor = new Tone.Compressor({
-    threshold: -6,
-    ratio: 3,
-    attack: 0.5,
-    release: 0.1
-  });
-  // const disto = new Tone.Distortion(0.1);
-  const freeverb = new Tone.Freeverb(0.9);
-  // freeverb.dampening.value = 500;
-  Tone.Master.chain(freeverb, masterCompressor);
+function MasterOut() {
+  const [reverbLoaded, setReverbLoaded] = useState(false);
+  const [compressor, setCompressor] = useState(null);
+  const [disto, setDisto] = useState(null);
+  const [reverb, setReverb] = useState(null);
+  const [allLoaded, setAllLoaded] = useState(false);
+
+  useEffect(() => {
+    console.log("Master effect...", allLoaded);
+    if (!reverbLoaded) {
+      setCompressor(
+        new Tone.Compressor({
+          threshold: -6,
+          ratio: 3,
+          attack: 0.5,
+          release: 0.1
+        })
+      );
+      setDisto(new Tone.Distortion(0.9));
+      const verb = new Tone.Reverb(0.9);
+      verb.preDelay = 0.05;
+      verb.generate().then(() => {
+        setReverb(verb);
+        setReverbLoaded(true);
+      });
+    }
+    if (!allLoaded && reverbLoaded) {
+      disto.wet.value = 0;
+      reverb.wet.value = 0.5;
+      Tone.Master.chain(disto, reverb, compressor);
+      setAllLoaded(true);
+    }
+  }, [reverbLoaded, compressor, disto, reverb, allLoaded]);
+
   return null;
-};
+}
 
 export default MasterOut;
