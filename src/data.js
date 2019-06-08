4014eaad756ad22dd82b5e80b84e5c9ee7aee030
E1P <ewan.pearce@gmail.com>
2019-06-08 01:33:06 +0100

    Create data for sounds with keys and notes

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const sounds = [
+  {
+    key: "a",
+    file: "kick",
+    note: "C3"
+  },
+  {
+    key: "s",
+    file: "snare",
+    note: "D3"
+  },
+  {
+    key: "d",
+    file: "hat-open",
+    note: "E3"
+  },
+  {
+    key: "f",
+    file: "hat-closed",
+    note: "F3"
+  },
+  {
+    key: "g",
+    file: "shaker",
+    note: "G3"
+  },
+  {
+    key: "h",
+    file: "tamb",
+    note: "A4"
+  },
+  {
+    key: "j",
+    file: "cymbal",
+    note: "B4"
+  },
+  {
+    key: "k",
+    file: "stab",
+    note: "C4"
+  },
+  {
+    key: "l",
+    file: "bass",
+    note: "D4"
+  }
+];
+
+export { sounds };
