60519df925b36155fcacfdfc501961030b06f06a
E1P <ewan.pearce@gmail.com>
2019-06-08 01:34:33 +0100

    Refactor component moving all audio Sampler logic out

--- src/App.js
+++ src/App.js
@@ -1,56 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Tone from "tone";
-import LevelMeter from "./Components/LevelMeter";
+import Sampler from "./Components/Sampler";
+import MasterOut from "./Components/MasterOut";
+import { sounds } from "./data";
 
 function App() {
-  const [amplitude, setAmplitude] = useState(-50);
-  const [playing, setPlaying] = useState(false);
-  const [Sampler, setSampler] = useState(null);
-  const [Meter, setMeter] = useState(null);
-
-  useEffect(() => {
-    console.log("Sampler useEffect triggered...");
-    !Sampler && setSampler(new Tone.Sampler({ C3: "./beat-radar-sounds/kick.wav" }).toMaster());
-    !Meter && setMeter(new Tone.Meter());
-    Sampler && Meter && Sampler.connect(Meter);
-  }, [Sampler, Meter]);
-
-  const playSound = note => {
-    Sampler.triggerAttack("C3");
-  };
+  const [triggers, setTriggers] = useState({
+    a: false,
+    s: false,
+    d: false,
+    f: false,
+    g: false,
+    h: false,
+    j: false,
+    k: false,
+    l: false
+  });
 
   const handleKeyDown = event => {
-    if (!playing && event.key === "a") {
-      Sampler.triggerAttack("C3");
-      setPlaying(true);
-    }
+    const newTriggers = { ...triggers };
+    newTriggers[event.key] = true;
+    setTriggers(newTriggers);
   };
 
   const handleKeyUp = event => {
-    if (playing && event.key === "a") {
-      Sampler.triggerRelease("C3");
-      setPlaying(false);
-    }
+    const newTriggers = { ...triggers };
+    newTriggers[event.key] = false;
+    setTriggers(newTriggers);
   };
-
-  useEffect(() => {
-    const getLevel = () => {
-      const level = Math.trunc(Meter.getLevel());
-      if (level > -50) setAmplitude(level);
-      else if (amplitude !== -50 && level < -50) setAmplitude(-50);
-      requestAnimationFrame(getLevel);
-    };
-    Meter && requestAnimationFrame(getLevel);
-    // return;
-  }, [Meter, amplitude]);
 
   return (
     <div className="App" onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex={0}>
-      <div className="channel">
-        <button onClick={playSound}>BEATS!</button>
-        <LevelMeter level={amplitude} />
-      </div>
+      {sounds.map((sound, index) => {
+        return <Sampler key={index} file={sound.file} note={sound.note} trigger={triggers[sound.key]} />;
+      })}
+      <MasterOut />
     </div>
   );
 }
