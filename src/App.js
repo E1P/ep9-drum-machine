6dffdb427da87fbf6e78e4415b2886138d5c4490
E1P <ewan.pearce@gmail.com>
2019-06-08 20:38:50 +0100

    Remove unnecessary triggers and event-handlers. Deconstruct data in map.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Sampler from "./Components/Sampler";
 import MasterOut from "./Components/MasterOut";
 import { sounds } from "./data";
 
 function App() {
-  const [triggers, setTriggers] = useState({
-    a: false,
-    s: false,
-    d: false,
-    f: false,
-    g: false,
-    h: false,
-    j: false,
-    k: false,
-    l: false
-  });
-
-  const handleKeyDown = event => {
-    const newTriggers = { ...triggers };
-    newTriggers[event.key] = true;
-    setTriggers(newTriggers);
-  };
-
-  const handleKeyUp = event => {
-    const newTriggers = { ...triggers };
-    newTriggers[event.key] = false;
-    setTriggers(newTriggers);
-  };
-
   return (
-    <div className="App" onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex={0}>
+    <div className="App">
       {sounds.map((sound, index) => {
-        return <Sampler key={index} file={sound.file} note={sound.note} trigger={triggers[sound.key]} />;
+        const { file, note, key, release } = sound;
+        return <Sampler key={index} file={file} note={note} trigger={key} release={release} />;
       })}
       <MasterOut />
     </div>
