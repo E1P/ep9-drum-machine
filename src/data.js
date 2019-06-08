ebe2a78976b77720a9bf800c2c6675ba7ba1c681
E1P <ewan.pearce@gmail.com>
2019-06-08 20:39:27 +0100

    Add release boolean to each key

--- src/data.js
+++ src/data.js
@@ -2,47 +2,56 @@ const sounds = [
   {
     key: "a",
     file: "kick",
-    note: "C3"
+    note: "C3",
+    release: false
   },
   {
     key: "s",
     file: "snare",
-    note: "D3"
+    note: "D3",
+    release: false
   },
   {
     key: "d",
     file: "hat-open",
-    note: "E3"
+    note: "E3",
+    release: true
   },
   {
     key: "f",
     file: "hat-closed",
-    note: "F3"
+    note: "F3",
+    release: false
   },
   {
     key: "g",
     file: "shaker",
-    note: "G3"
+    note: "G3",
+    release: false
   },
   {
     key: "h",
     file: "tamb",
-    note: "A4"
+    note: "A4",
+    release: false
   },
   {
     key: "j",
     file: "cymbal",
-    note: "B4"
+    note: "B4",
+    release: false
   },
   {
     key: "k",
     file: "stab",
-    note: "C4"
+    note: "C4",
+    release: true
   },
   {
     key: "l",
     file: "bass",
-    note: "D4"
+    note: "D4",
+    release: true
   }
 ];
 
