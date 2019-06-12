f46c9f4d7fcee7a5fd67a86f12a23b8683df0d76
E1P <ewan.pearce@gmail.com>
2019-06-12 23:49:26 +0100

    Fix height calculation.

--- src/Components/FreqBox/FreqBox.jsx
+++ src/Components/FreqBox/FreqBox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./FreqBox.css";
 
 function FreqBox({ freq }) {
-  freq = Math.trunc(freq * 100);
+  freq = 1 + Math.trunc(99 + freq);
   const height = `${freq}%`;
   return <div className="freq-box" style={{ height: height }} />;
 }
