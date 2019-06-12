447d07c8d86e2092f993f016f3a1abe7d05b261c
E1P <ewan.pearce@gmail.com>
2019-06-12 02:17:10 +0100

    Add container for keypad and visualiser.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import "./App.css";
 import Sampler from "./Components/Sampler/Sampler";
 import MasterOut from "./Components/MasterOut/MasterOut";
-// import Visualiser from "./Components/Visualiser/Visualiser";
+import Visualiser from "./Components/Visualiser/Visualiser";
 import { sounds } from "./data";
 
 function App() {
   return (
     <div className="App">
-      <div className="drum-pad">
-        {sounds.map((sound, index) => {
-          const { file, note, key, release } = sound;
-          return <Sampler key={index} file={file} note={note} trigger={key} release={release} />;
-        })}
+      <div className="main-container">
+        <div className="drum-pad">
+          {sounds.map((sound, index) => {
+            const { file, note, key, release } = sound;
+            return <Sampler key={index} file={file} note={note} trigger={key} release={release} />;
+          })}
+        </div>
+        <MasterOut />
+        <Visualiser />
       </div>
-      <MasterOut />
-      {/* <Visualiser /> */}
     </div>
   );
 }
