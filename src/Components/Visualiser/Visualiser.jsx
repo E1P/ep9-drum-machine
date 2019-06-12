b7dde9f930a3751aaa0c1b397f850096fbce756e
E1P <ewan.pearce@gmail.com>
2019-06-12 02:18:59 +0100

    Set up animation for values received from waveform

--- src/Components/Visualiser/Visualiser.jsx
+++ src/Components/Visualiser/Visualiser.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Tone from "tone";
+import "./Visualiser.css";
+import FreqBox from "../FreqBox/FreqBox";
 
 function Visualiser() {
-  const waveform = new Tone.Waveform();
-  const master = Tone.Master();
-  waveform.connect(master);
+  const initialValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const [waveform, setWaveform] = useState(null);
+  const [values, setValues] = useState(initialValues);
 
-  return <div className="waveform" onClick={() => console.log(waveform)} />;
+  useEffect(() => {
+    if (!waveform) {
+      setWaveform(new Tone.Waveform(32));
+    }
+    waveform && waveform.receive("waveform");
+  }, [waveform]);
+
+  useEffect(() => {
+    let animationId;
+    const getValues = () => {
+      const newValues = waveform.getValue();
+      setValues(Array.from(newValues));
+      requestAnimationFrame(getValues);
+    };
+    if (waveform) {
+      animationId = requestAnimationFrame(getValues);
+    }
+    return () => cancelAnimationFrame(animationId);
+  }, [values, waveform]);
+
+  return (
+    <div className="waveform">
+      {values.map((freq, index) => {
+        return <FreqBox key={index} freq={freq} />;
+      })}
+    </div>
+  );
 }
 
 export default Visualiser;
