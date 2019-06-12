7fdae36d7f713d0d6cdc11d2b865cfc058c63274
E1P <ewan.pearce@gmail.com>
2019-06-12 23:53:45 +0100

    Change analysis to FFT and make click-toggle for visualisation.

--- src/Components/Visualiser/Visualiser.jsx
+++ src/Components/Visualiser/Visualiser.jsx
@@ -1,38 +1,50 @@
 import React, { useState, useEffect } from "react";
 import Tone from "tone";
+import { initialFFTArray } from "../../data";
 import "./Visualiser.css";
 import FreqBox from "../FreqBox/FreqBox";
 
 function Visualiser() {
-  const initialValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   const [waveform, setWaveform] = useState(null);
-  const [values, setValues] = useState(initialValues);
+  const [values, setValues] = useState(initialFFTArray);
+  const [enabled, setEnabled] = useState(false);
 
   useEffect(() => {
+    console.log("setting waveform");
     if (!waveform) {
-      setWaveform(new Tone.Waveform(32));
+      setWaveform(new Tone.FFT(32));
+      console.log("waveform node >>> ", waveform);
     }
     waveform && waveform.receive("waveform");
   }, [waveform]);
 
+  const handleClick = () => {
+    setEnabled(!enabled);
+    console.log(enabled, `Visualiser ${enabled ? "on" : "off"}`);
+  };
+
   useEffect(() => {
     let animationId;
     const getValues = () => {
-      const newValues = waveform.getValue();
-      setValues(Array.from(newValues));
+      const newValues = Array.from(waveform.getValue());
+      setValues(newValues);
       requestAnimationFrame(getValues);
     };
-    if (waveform) {
-      animationId = requestAnimationFrame(getValues);
-    }
-    return () => cancelAnimationFrame(animationId);
-  }, [values, waveform]);
+    if (waveform && enabled) animationId = requestAnimationFrame(getValues);
+    if (animationId)
+      return () => {
+        cancelAnimationFrame(animationId);
+      };
+  }, [values, waveform, enabled]);
 
   return (
-    <div className="waveform">
-      {values.map((freq, index) => {
-        return <FreqBox key={index} freq={freq} />;
-      })}
+    <div className="waveform" onClick={handleClick}>
+      {!enabled && <p>Click to toggle audio visualiser</p>}
+      {enabled &&
+        values.map((freq, index) => {
+          freq = freq > -99 ? freq : -99;
+          return <FreqBox key={index} freq={freq} />;
+        })}
     </div>
   );
 }
