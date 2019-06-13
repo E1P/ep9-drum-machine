e373cc24743c527f974e15487baf99dad3c94e06
E1P <ewan.pearce@gmail.com>
2019-06-13 14:57:30 +0100

    Adjust line position. Raise click handler and state to App.

--- src/Components/LineVisualiser/LineVisualiser.jsx
+++ src/Components/LineVisualiser/LineVisualiser.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import Tone from "tone";
 import "./LineVisualiser.css";
 
-function LineVisualiser() {
+function LineVisualiser({ enabled, handleVisualiserClick }) {
   const [waveform, setWaveform] = useState(null);
   const [freqLine, setFreqLine] = useState("350 650");
-  const [enabled, setEnabled] = useState(false);
+  // const [enabled, setEnabled] = useState(false);
 
   useEffect(() => {
     console.log("setting waveform");
@@ -16,10 +16,10 @@ function LineVisualiser() {
     waveform && waveform.receive("waveform");
   }, [waveform]);
 
-  const handleClick = () => {
-    setEnabled(!enabled);
-    console.log(enabled, `Visualiser ${enabled ? "on" : "off"}`);
-  };
+  // const handleClick = () => {
+  //   setEnabled(!enabled);
+  //   console.log(enabled, `Visualiser ${enabled ? "on" : "off"}`);
+  // };
 
   useEffect(() => {
     let animationId;
@@ -27,12 +27,12 @@ function LineVisualiser() {
       const values = Array.from(waveform.getValue());
       const lineString = values
         .map((freq, index) => {
-          const y = freq > -99 ? 350 - (Math.trunc(freq) + 99) * 3.5 : 350;
+          const y = freq > -99 ? 350 - (Math.trunc(freq) + 99) * 3.5 : 340;
           const x = index * 20 + 7;
           return `${x},${y}`;
         })
         .join(" ");
-      setFreqLine(`0,350 ${lineString} 650, 350`);
+      setFreqLine(`1,340 ${lineString} 637, 340`);
       requestAnimationFrame(getValues);
     };
     if (waveform && enabled) animationId = requestAnimationFrame(getValues);
@@ -43,11 +43,11 @@ function LineVisualiser() {
   }, [waveform, enabled]);
 
   return (
-    <div className="waveform" onClick={handleClick}>
+    <div className="waveform" onClick={handleVisualiserClick}>
       {!enabled && <p>Click to toggle audio visualiser</p>}
       {enabled && (
         <div className="waveform">
-          <svg width="650" height="350">
+          <svg width="646" height="346">
             <polyline
               points={freqLine}
               shapeRendering="auto"
@@ -56,7 +56,7 @@ function LineVisualiser() {
               fillRule="nonzero"
               stroke="white"
               stroke-width="5"
-              stroke-linejoin="round"
+              // stroke-linejoin="round"
             />
           </svg>
         </div>
