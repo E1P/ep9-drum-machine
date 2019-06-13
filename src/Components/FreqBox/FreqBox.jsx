300909da99f64a28a1976729992135b30f72870d
E1P <ewan.pearce@gmail.com>
2019-06-13 16:47:47 +0100

    Tidy-up height calculation.

--- src/Components/FreqBox/FreqBox.jsx
+++ src/Components/FreqBox/FreqBox.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import "./FreqBox.css";
 
 function FreqBox({ freq }) {
-  freq = 1 + Math.trunc(99 + freq);
-  const height = `${freq}%`;
+  const height = `${1 + Math.trunc(99 + freq)}%`;
   return <div className="freq-box" style={{ height: height }} />;
 }
 
