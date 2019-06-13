98d3c3a31c5767c623bfeeeb78a4b1f2ea1e7823
E1P <ewan.pearce@gmail.com>
2019-06-13 14:55:04 +0100

    Raise up visualiser enabled/type state and handler functions to enable access for controls component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Sampler from "./Components/Sampler/Sampler";
 import MasterOut from "./Components/MasterOut/MasterOut";
-// import Visualiser from "./Components/Visualiser/Visualiser";
+import Controls from "./Components/Controls/Controls";
+import Visualiser from "./Components/Visualiser/Visualiser";
 import LineVisualiser from "./Components/LineVisualiser/LineVisualiser";
 import { sounds } from "./data";
 
 function App() {
+  const [visualiser, setVisualiser] = useState("box"); // or "line"
+  const [enabled, setEnabled] = useState(false);
+
+  const handleVisualiserChange = () => {
+    setVisualiser(visualiser === "box" ? "line" : "box");
+    setEnabled(enabled);
+  };
+
+  const handleVisualiserClick = () => {
+    setEnabled(!enabled);
+  };
+
   return (
     <div className="App">
       <div className="main-container">
@@ -16,9 +29,10 @@ function App() {
             return <Sampler key={index} file={file} note={note} trigger={key} release={release} />;
           })}
         </div>
+        <Controls visualiser={visualiser} handleVisualiserChange={handleVisualiserChange} />
         <MasterOut />
-        {/* <Visualiser /> */}
-        <LineVisualiser />
+        {visualiser === "box" && <Visualiser handleVisualiserClick={handleVisualiserClick} enabled={enabled} />}
+        {visualiser === "line" && <LineVisualiser handleVisualiserClick={handleVisualiserClick} enabled={enabled} />}
       </div>
     </div>
   );
