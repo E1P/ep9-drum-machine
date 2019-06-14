5a120206709fbfb93913094a56446d2167fc086d
E1P <ewan.pearce@gmail.com>
2019-06-14 19:04:00 +0100

    Fine-tune line position and add gradient to svg fill.

--- src/Components/LineVisualiser/LineVisualiser.jsx
+++ src/Components/LineVisualiser/LineVisualiser.jsx
@@ -4,6 +4,7 @@ import "./LineVisualiser.css";
 
 function LineVisualiser({ enabled, handleVisualiserClick }) {
   const [waveform, setWaveform] = useState(null);
+  const [valuesArray, setValuesArray] = useState([]);
   const [freqLine, setFreqLine] = useState("350 650");
 
   useEffect(() => {
@@ -17,14 +18,16 @@ function LineVisualiser({ enabled, handleVisualiserClick }) {
     let animationId;
     const getValues = () => {
       const values = Array.from(waveform.getValue());
+      setValuesArray([values, ...valuesArray.slice(0, 2)]);
+      // get average values from valuesArray
       const lineString = values
         .map((freq, index) => {
-          const y = freq > -99 ? 350 - (Math.trunc(freq) + 99) * 3.5 : 340;
-          const x = index * 20 + 7;
+          const y = freq > -99 ? 350 - (Math.trunc(freq) + 100) * 3.5 : 344;
+          const x = index * 20.51 + 4;
           return `${x},${y}`;
         })
         .join(" ");
-      setFreqLine(`1,340 ${lineString} 637, 340`);
+      setFreqLine(`5,345 ${lineString} 641,345 5,345`);
       requestAnimationFrame(getValues);
     };
     if (waveform && enabled) animationId = requestAnimationFrame(getValues);
@@ -32,15 +35,30 @@ function LineVisualiser({ enabled, handleVisualiserClick }) {
       return () => {
         cancelAnimationFrame(animationId);
       };
-  }, [waveform, enabled]);
+  }, [waveform, enabled, valuesArray]);
 
   return (
     <div className="waveform" onClick={handleVisualiserClick}>
       {!enabled && <p>Click to toggle audio visualiser</p>}
       {enabled && (
-        <div className="waveform">
-          <svg width="646" height="346">
-            <polyline points={freqLine} shapeRendering="auto" fill="white" fillOpacity="0.5" stroke="white" strokeWidth="5" />
+        <div /* className="waveform" */>
+          <svg width="646" height="345" className="svg-container">
+            <defs>
+              <linearGradient id="fill-gradient" x1="0" x2="0" y1="1" y2="0">
+                {/* gradient co-ords */}
+                <stop offset="0" stop-color="white" />
+                <stop offset="1" stop-color="" />
+              </linearGradient>
+            </defs>
+            <polyline
+              className="polyline-one"
+              points={freqLine}
+              shapeRendering="auto"
+              fill="url(#fill-gradient)"
+              fillOpacity="0.5"
+              stroke="url(#fill-gradient)"
+              strokeWidth="1"
+            />
           </svg>
         </div>
       )}
