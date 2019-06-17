05c12cd1ea83191d1570b8909b3809bcb9032a28
E1P <ewan.pearce@gmail.com>
2019-06-17 12:45:46 +0100

    Move header code into component.

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <header className="header">EP-9 Drum Machine</header>
+      <div className="menu-container" />
+    </div>
+  );
+};
+
+export default Header;
