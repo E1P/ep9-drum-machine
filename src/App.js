aff0b7eaee14aeffe519aa7dbcf536c93d1dc523
E1P <ewan.pearce@gmail.com>
2019-06-18 23:37:13 +0100

    Refactor isMobile conditional so each option returns App div.

--- src/App.js
+++ src/App.js
@@ -30,34 +30,34 @@ function App() {
     setMenuIsVisible(!menuIsVisible);
   };
 
-  return (
+  return isMobile ? (
+    <div className="App">
+      <div className="non-desktop">
+        <p className="app-text">Desktop only. Sorry.</p>
+        <img src={image} alt="Drum machine" width="90%" />
+      </div>
+    </div>
+  ) : (
     <div className="App" onClick={menuIsVisible ? () => setMenuIsVisible(false) : null}>
-      {isMobile ? (
-        <div className="non-desktop">
-          <p className="app-text">Desktop only. Sorry.</p>
-          <img src={image} alt="Drum machine" width="90%" />
-        </div>
-      ) : (
-        <div className="main-container">
-          <Header handleMenuToggle={handleMenuToggle} visible={menuIsVisible} />
-          <div className={`machine-container`}>
-            <div className="drum-pad">
-              {sounds.map((sound, index) => {
-                const { file, note, key, release } = sound;
-                return <Sampler key={index} file={file} note={note} trigger={key} release={release} />;
-              })}
-            </div>
-            <div className="master-controls-box">
-              <Controls visualiser={visualiser} handleVisualiserChange={handleVisualiserChange} />
-              <div className="controls-seam-filler">{}</div>
-              <MasterOut />
-            </div>
-            {visualiser === "box" && <Visualiser handleVisualiserClick={handleVisualiserClick} enabled={enabled} />}
-            {visualiser === "line" && <LineVisualiser handleVisualiserClick={handleVisualiserClick} enabled={enabled} />}
-            <MenuModal visible={menuIsVisible} />
+      <div className="main-container">
+        <Header handleMenuToggle={handleMenuToggle} visible={menuIsVisible} />
+        <div className={`machine-container`}>
+          <div className="drum-pad">
+            {sounds.map((sound, index) => {
+              const { file, note, key, release } = sound;
+              return <Sampler key={index} file={file} note={note} trigger={key} release={release} />;
+            })}
           </div>
+          <div className="master-controls-box">
+            <Controls visualiser={visualiser} handleVisualiserChange={handleVisualiserChange} />
+            <div className="controls-seam-filler">{}</div>
+            <MasterOut />
+          </div>
+          {visualiser === "box" && <Visualiser handleVisualiserClick={handleVisualiserClick} enabled={enabled} />}
+          {visualiser === "line" && <LineVisualiser handleVisualiserClick={handleVisualiserClick} enabled={enabled} />}
         </div>
-      )}
+      </div>
+      <MenuModal visible={menuIsVisible} />
     </div>
   );
 }
